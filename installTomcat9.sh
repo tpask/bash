d9e6b3fff89b1e8e8f8bae28b53f1dae00ebdb28
Thien <hairyk9@gmail.com>
2018-12-26 08:58:32 -0800

    Create installTomcat9.sh

---
+++ installTomcat9.sh
@@ -0,0 +1,5 @@
+#script to install tomcat.
+  wget http://www-us.apache.org/dist/tomcat/tomcat-9/v9.0.14/bin/apache-tomcat-9.0.14.tar.gz
+  tar -xvf apache-tomcat-9.0.14.tar.gz 
+  mv apache-tomcat-9.0.14 tomcat
+  ln -s tomcat tomcat9
