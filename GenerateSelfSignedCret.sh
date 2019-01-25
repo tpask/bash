1784e5d49a529aea010f92823491d7692e88e87c
Thien <hairyk9@gmail.com>
2019-01-25 09:28:22 -0800

    Update GenerateSelfSignedCret.sh

--- GenerateSelfSignedCret.sh
+++ GenerateSelfSignedCret.sh
@@ -1,10 +1,10 @@
 #sample script to generate self signed cert
+# fill cert info prior to running script
   
 function genCert {
 # Set our CSR variables
-  SSL_DIR="/etc/ssl/private"
+  SSL_DIR="../certs"
   certName="selfSignedCert"
-  certDir="/etc/ssl/certs"
   DOMAIN="*.myDomain.com"
   PASSPHRASE=""
   
@@ -22,8 +22,8 @@ mkdir -p "$SSL_DIR"
 # Generate our Private Key, CSR and Certificate
 openssl genrsa -out "$SSL_DIR/$certName.key" 2048
 openssl req -new -subj "$(echo -n "$SUBJ" | tr "\n" "/")" -key "$SSL_DIR/$certName.key" -out "$SSL_DIR/$certName.csr" -passin pass:$PASSPHRASE
-openssl x509 -req -days 365 -in "$SSL_DIR/$certName.csr" -signkey "$SSL_DIR/$certName.key" -out $certDir/$certName.crt
-openssl dhparam -out $certDir/$certName.pem 2048                                                            
+openssl x509 -req -days 365 -in "$SSL_DIR/$certName.csr" -signkey "$SSL_DIR/$certName.key" -out $SSL_DIR/$certName.crt
+openssl dhparam -out $SSL_DIR/$certName.pem 2048                                                            
 }
 
 ##
